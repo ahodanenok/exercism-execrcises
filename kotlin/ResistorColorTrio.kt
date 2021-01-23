ea6295e4b4be601c6728822718cf6488ce38d688
Andrei Hodanenok <xoda91@gmail.com>
2021-01-23 22:13:17 +0300

    Exercise: Resistor Color Trio

--- kotlin/ResistorColorTrio.kt
+++ kotlin/ResistorColorTrio.kt
@@ -3,11 +3,12 @@
  * Resistor Color Trio
  */
 
+import kotlin.math.log10
 import kotlin.math.pow
 
 object ResistorColorTrio {
 
-    fun text(vararg input: Color): String {
+    fun text_1(vararg input: Color): String {
         if (input.size < 3) {
             throw IllegalArgumentException("Provide at least 3 colors")
         }
@@ -28,9 +29,29 @@ object ResistorColorTrio {
 
         val unit = Unit.values().findLast { it.exp <= exp }
                 ?: throw IllegalStateException("Unsupported ohms value with $exp zeroes")
-        val multiplier = 10.toFloat().pow(exp - unit.exp).toInt()
+        val multiplier = 10.0.pow(exp - unit.exp).toInt()
 
         return "${base * multiplier} ${unit.name.toLowerCase()}"
+    }
+
+    fun text_2(vararg input: Color): String {
+        if (input.size < 3) {
+            throw IllegalArgumentException("Provide at least 3 colors")
+        }
+
+        val first = input[0]
+        val second = input[1]
+        val third = input[2]
+
+        // find total value and count number of zeroes
+        val ohms = (first.code * 10 + second.code) * 10.0.pow(third.code)
+        val exp = log10(ohms)
+
+        val unit = Unit.values().findLast { it.exp <= exp }
+                ?: throw IllegalStateException("Unsupported ohms value with $exp zeroes")
+        val base = ohms / 10.0.pow(unit.exp)
+
+        return "${base.toInt()} ${unit.name.toLowerCase()}"
     }
 
     enum class Color(val code: Int) {
