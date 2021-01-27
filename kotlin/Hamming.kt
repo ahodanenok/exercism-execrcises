46e669a80cdb341da8ebc53f836f0f22d176170a
Andrei Hodanenok <xoda91@gmail.com>
2021-01-27 23:20:28 +0300

    Exercise: Hamming

---
+++ kotlin/Hamming.kt
@@ -0,0 +1,15 @@
+/**
+ * https://exercism.io/my/tracks/kotlin
+ * Hamming
+ */
+
+object Hamming {
+
+    fun compute(leftStrand: String, rightStrand: String): Int {
+        if (leftStrand.length != rightStrand.length) {
+            throw IllegalArgumentException("left and right strands must be of equal length")
+        }
+
+        return leftStrand.zip(rightStrand).count { it.first != it.second }
+    }
+}
