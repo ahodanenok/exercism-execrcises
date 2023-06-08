21a0b8c5d3912819ba196a6fc37d438327542f91
Andrei Hodanenok <xoda91@gmail.com>
2023-06-08 22:52:44 +0300

    Java: Raindrops

---
+++ java/raindrops/RaindropConverter.java
@@ -0,0 +1,28 @@
+/**
+ * Raindrops
+ * https://exercism.org/tracks/java/exercises/raindrops
+ */
+class RaindropConverter {
+
+    String convert(int number) {
+        String result = "";
+
+        if (number % 3 == 0) {
+            result += "Pling";
+        }
+
+        if (number % 5 == 0) {
+            result += "Plang";
+        }
+
+        if (number % 7 == 0) {
+            result += "Plong";
+        }
+
+        if (result.isEmpty()) {
+            result += number;
+        }
+
+        return result;
+    }
+}
